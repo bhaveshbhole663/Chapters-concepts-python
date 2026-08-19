8ae59817220c3483d5370d78b27b5b6ed7789b8f
Bhavesh Bhole <bhaveshbhole663@gmail.com>
2026-08-19 22:06:12 +0530

    ch 6 concepts

---
+++ ch 6/01_conditionals.py
@@ -0,0 +1,13 @@
+a = int(input("Enter your age:"))
+
+# If else lader 
+
+if(a>18):
+    print("Your are above the age of consent")
+
+
+
+else:
+    print("Your are not above the age of consent")
+    
+    
---
+++ ch 6/02_if_elif_else_lader.py
@@ -0,0 +1,18 @@
+
+
+a = int(input("Enter your age:"))
+
+# If else lader 
+
+if(a>18):
+    print("Your are above the age of consent")
+
+
+elif(a<=0):
+    print("You are entering an invalid age")
+
+else:
+    print("Your are not above the age of consent")
+    
+    
+
